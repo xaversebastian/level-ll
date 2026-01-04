272f4011b65380f01425b1f14c3337ee9b6a5aca
Xaver <xaverfreytag@icloud.com>
2026-01-04 20:52:30 +0100

    Initial Commit

---
+++ LevelEleven/LevelElevenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LevelElevenApp.swift
+//  LevelEleven
+//
+//  Created by Xaver Freytag on 04/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct LevelElevenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LevelElevenTests/LevelElevenTests.swift
@@ -0,0 +1,17 @@
+//
+//  LevelElevenTests.swift
+//  LevelElevenTests
+//
+//  Created by Xaver Freytag on 04/01/2026.
+//
+
+import Testing
+@testable import LevelEleven
+
+struct LevelElevenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
